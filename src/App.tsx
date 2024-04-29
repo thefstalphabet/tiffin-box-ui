bff62d3beb561c86a5afb07c658aa86baa2cf13a
Akash Patel <69709410+thefstalphabet@users.noreply.github.com>
2024-04-29 18:24:29 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "antd";
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
