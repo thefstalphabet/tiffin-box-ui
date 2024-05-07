672eb96ac35b8693141eab93996e7dfefb8be5e2
Deependra jhariya <107026547+Deependra-jhariya@users.noreply.github.com>
2024-05-07 08:36:29 +0530

    update:remove some menuitems

--- src/Configs/MenuItems.tsx
+++ src/Configs/MenuItems.tsx
@@ -72,45 +72,4 @@ export const items: ItemType<MenuItemType>[] = [
       },
     ],
   },
-  {
-    type: "divider",
-  },
-  {
-    key: "sub4",
-    label: "Navigation Three",
-    icon: <SettingOutlined />,
-    children: [
-      {
-        key: "9",
-        label: "Option 9",
-      },
-      {
-        key: "10",
-        label: "Option 10",
-      },
-      {
-        key: "11",
-        label: "Option 11",
-      },
-      {
-        key: "12",
-        label: "Option 12",
-      },
-    ],
-  },
-  {
-    key: "grp",
-    label: "Group",
-    type: "group",
-    children: [
-      {
-        key: "13",
-        label: "Option 13",
-      },
-      {
-        key: "14",
-        label: "Option 14",
-      },
-    ],
-  },
 ];
