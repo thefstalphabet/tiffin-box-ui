43b107edc9bbf8b8c849ae169e014fe61b1e5884
Deependra jhariya <107026547+Deependra-jhariya@users.noreply.github.com>
2024-05-11 11:33:46 +0530

    update:"fix button"
    done

--- src/Routes/Kitchen/Kitchen.tsx
+++ src/Routes/Kitchen/Kitchen.tsx
@@ -1,25 +1,19 @@
 import React from "react";
-import landingImg from "../../Assets/Images/landingImg.jpg"; // Importing the JPG image
+import landingImg from "../../Assets/Images/landingImg.jpg"; 
 import { Button } from "antd";
 export default function Kitchen() {
   return (
     <div className="flex h-screen">
-      {" "}
-      {/* Set height to full viewport height */}
       <div
         className="bg-cover bg-center flex justify-center w-full"
         style={{ backgroundImage: `url(${landingImg})`, height: "100%" }}
       >
         <div className="flex items-center">
-          {" "}
-          {/* Center vertically */}
           <div className="text-left  w-3/6">
-            {" "}
-            {/* Align text to the left */}
             <h2 className="text-white text-2xl sm:text-3xl lg:text-4xl xl:text-5xl mx-10 font-bold">
             Join Our Kitchen Network: Deliver Delicious Meals to Busy Customers
             </h2>
-            <Button className="mx-10 my-5" type="primary" size="large" shape ="round"  >Signup</Button>
+            <Button className="mx-10 my-5" type="primary" size="large">Registration</Button>
           </div>
         </div>
       </div>
