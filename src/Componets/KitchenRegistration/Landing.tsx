5913e56edf78aa7f94ee35fb857556a8574e44e8
Akash patel <akashpatel10000@gmail.com>
2024-05-14 10:04:50 +0530

    Update: layout fix

--- src/Componets/KitchenRegistration/Landing.tsx
+++ src/Componets/KitchenRegistration/Landing.tsx
@@ -2,16 +2,11 @@ import { kitchenImg } from "../../Assets";
 import { Button } from "antd";
 export default function Landing() {
   return (
-    <div
-      className="subContainer my-10 h-89.5vh grid grid-cols-auto-flow justify-evenly items-center"
-      style={{}}
-    >
-      <div className="grid gap-6" style={{ justifyItems: "left" }}>
+    <div className="subContainer my-10 h-89.5vh grid grid-cols-1 justify-items-center gap-10 items-center md:grid-cols-2 lg:gap-0">
+      <div className="grid gap-6 justify-items-center md:justify-items-start text-center lg:text-left md:text-left">
         <div>
           <h1 className="h1 font-semibold">Join Our Kitchen Network</h1>
-          <div>
-            <p>Deliver Delicious Meals to Busy Customers.</p>
-          </div>
+          <h3>Deliver Delicious Meals to Busy Customers.</h3>
         </div>
         <Button type="primary" size="large">
           Start Today
