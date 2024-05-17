fbc2ce1a711994e95733ce28cf3a656bc35b9238
Deependra jhariya <107026547+Deependra-jhariya@users.noreply.github.com>
2024-05-17 09:21:18 +0530

    update:create login form

--- src/Componets/LoginForm/LoginForm.tsx
+++ src/Componets/LoginForm/LoginForm.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import * as Styles from "./LoginFormStyles";
-import { Button, Form } from "antd";
+import { Button, Form, Card } from "antd";
 import ReForm from "../../reusable-antd-components/ReForm";
 import ReInput from "../../reusable-antd-components/ReFormFields/ReInput";
 import { chef } from "../../Assets";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faKitchenSet, faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 export default function LoginForm() {
   const [userType, setUserType] = useState("");
   const [form] = Form.useForm();
@@ -13,62 +14,87 @@ export default function LoginForm() {
   }
   return (
     <Styles.Container>
-      <ReForm formInstance={form} onSubmit={handleFormSubmit}>
-        <h1>Choose Account type</h1>
-        <Styles.UserType>
-          <div
-            style={{
-              boxShadow:
-                userType === "Kitchen"
-                  ? "rgba(0, 187, 6, 0.16) 0px 1px 4px"
-                  : " rgba(0, 0, 0, 0.16) 0px 1px 4px",
-            }}
-            className="image_box"
-            onClick={() => {
-              setUserType("Kitchen");
-            }}
-          >
-            <img src={chef} alt="chef image" />
-            <h2>Kitchen</h2>
-          </div>
-          <div
-            style={{
-              boxShadow:
-                userType === "Kitchen"
-                  ? "rgba(0, 187, 6, 0.16) 0px 1px 4px"
-                  : " rgba(0, 0, 0, 0.16) 0px 1px 4px",
-            }}
-            className="image_box"
-            onClick={() => {
-              setUserType("User");
-            }}
-          >
-            <img src={chef} alt="chef image" />
-            <h2>User</h2>
-          </div>
-        </Styles.UserType>
-        <ReInput
-          label="Email"
-          placeholder="Enter your email"
-          name="email"
-          type="email"
-          required
-        />
-        <ReInput
-          label="Password"
-          placeholder="Enter your password"
-          name="password"
-          type="password"
-          required
-        />
-        <Styles.SignView>
-          <div className="account_container">
-            <h3>No account?</h3>
-            <p> Signup</p>
-          </div>
-          <Button type="primary" onClick={()=> {form.submit()}}>Login</Button>
-        </Styles.SignView>
-      </ReForm>
+      <div className="signup_account">
+        <h3>No account ?</h3>
+        <p>Sign up</p>
+      </div>
+      <Styles.FormContainer>
+        <ReForm formInstance={form} onSubmit={handleFormSubmit}>
+          <h1>Login</h1>
+          <h3>Choose Account type</h3>
+          <Styles.CardContainer>
+            <Card
+              style={{
+                textAlign: "center",
+                boxShadow:
+                  userType === "Kitchen"
+                    ? "rgba(0, 187, 6, 0.16) 0px 1px 4px"
+                    : " rgba(0, 0, 0, 0.16) 0px 1px 4px",
+                width: 210,
+              }}
+              onClick={() => {
+                setUserType("Kitchen");
+              }}
+            >
+              <FontAwesomeIcon icon={faKitchenSet} size="4x" />
+              <p>Kitchen</p>
+              {userType == "Kitchen" && (
+                <div className="customContextMenu">
+                  <FontAwesomeIcon icon={faCircleCheck} color="green" />
+                </div>
+              )}
+            </Card>
+            <Card
+              style={{
+                textAlign: "center",
+                boxShadow:
+                  userType === "User"
+                    ? "rgba(0, 187, 6, 0.16) 0px 2px 4px"
+                    : " rgba(0, 0, 0, 0.16) 0px 1px 4px",
+                width: 210,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              onClick={() => {
+                setUserType("User");
+              }}
+            >
+              <FontAwesomeIcon icon={faKitchenSet} size="4x" />
+              <p>User</p>
+              {userType == "User" && (
+                <div className="customContextMenu">
+                  <FontAwesomeIcon icon={faCircleCheck} color="green" />
+                </div>
+              )}
+            </Card>
+          </Styles.CardContainer>
+          <ReInput
+            label="Email"
+            placeholder="Enter your email"
+            name="email"
+            type="email"
+            required
+          />
+          <ReInput
+            label="Password"
+            placeholder="Enter your password"
+            name="password"
+            type="password"
+            required
+          />
+          <Styles.Action>
+            <p>Forgot Password ?</p>
+            <Button
+              type="primary"
+              onClick={() => {
+                form.submit();
+              }}
+            >
+              Login
+            </Button>
+          </Styles.Action>
+        </ReForm>
+      </Styles.FormContainer>
     </Styles.Container>
   );
 }
--- src/Componets/LoginForm/LoginFormStyles.ts
+++ src/Componets/LoginForm/LoginFormStyles.ts
@@ -1,83 +1,63 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  .signup_account {
+    display: flex;
+    justify-content: flex-end;
+    margin: 2rem;
+    p {
+      margin-left: 10px;
+      color: green;
+    }
+  }
+`;
+export const FormContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  align-self: center;
+  padding: 2rem;
+  margin: 0 auto; 
+  max-width: 600px; 
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  border-radius: 5px; 
   h1 {
-    align-items: center;
     text-align: center;
-    justify-content: center;
     color: green;
   }
-  h2{
-    margin-top: 1rem;
+  h3 {
+    text-align: center;
   }
-  .centered {
-    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-    padding: 2rem;
-    margin:3rem
-  }
-  .check_Icon{
-    position: absolute;
-    top: 24.5rem;
-    right: 46.5rem;
-}
 `;
-export const UserType = styled.div`
+
+export const CardContainer = styled.div`
   display: flex;
+  flex-wrap: wrap;
   gap: 10px;
   justify-content: center;
-  margin-top: 2rem;
-  img {
-    height: 18vh;
-    width: 10vw;
-    align-self: center;
-    align-items: center;
-    justify-content: center;
-  }
-  .image_box {
-    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 1rem;
-  }
-`;
-export const Tittle = styled.div`
-  margin-top: 2rem;
-  h3 {
-    align-self: center;
-    text-align: center;
-  }
+  margin-top: 1rem;
+  margin-bottom: 1rem;
   p {
-    display: flex;
-    align-self: center;
-    align-items: center;
-    justify-content: center;
+    margin-top: 1rem;
+  }
+  .customContextMenu {
+  display: flex;
+  position: absolute;
+  justify-content: flex-end;
+  align-items: flex-end;
+  text-align: end;
+  left: 11.5rem;
+}
+  @media (max-width: 1000px) {
+    width: 100%;
   }
 `;
-export const SignView = styled.div`
+export const Action = styled.div`
   display: grid;
-  grid-template-columns: 1fr auto; /* One column for text content, one column for the button */
+  grid-template-columns: 1fr auto;
   align-items: center;
   margin-top: 1.5rem;
-  .account_container {
-    text-align: left;
-    display: flex;
-  }
-
-  .account_container h3,
-  .account_container p {
-    margin: 0;
-  }
   p {
-    display: flex;
-    justify-content: center;
     color: green;
-    margin-left: 1rem;
-    align-self: center;
   }
 `;
