69bf567b1e82348cbbac40a025fc65640da55007
Deependra jhariya <107026547+Deependra-jhariya@users.noreply.github.com>
2024-05-20 22:44:23 +0530

    update add socialmedia icon

--- src/Configs/SocialMediaIcon.tsx
+++ src/Configs/SocialMediaIcon.tsx
@@ -1,40 +1,41 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faUser,
-  faPhone,
-  faAddressCard,
-  faFireBurner, 
-} from "@fortawesome/free-solid-svg-icons";
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faGoogle,
+  faYoutube
+} from "@fortawesome/free-brands-svg-icons";
 import { ISocialMedia } from "../Interfaces/Configs/SocialMediaIcon.interface";
 export const socialMediaIcon: Array<ISocialMedia> = [
   {
     key: "1",
     label: "Facebook",
-    icon: <FontAwesomeIcon icon={faAddressCard} color="white" />,
+    icon: <FontAwesomeIcon icon={faFacebook} color="white" size="2x" />,
     path: "/facebook",
   },
   {
     key: "2",
     label: "Instagram",
-    icon: <FontAwesomeIcon icon={faUser} color="white"  />,
-    path: "/instgram",
+    icon: <FontAwesomeIcon icon={faInstagram} color="white"  size="2x"/>,
+    path: "/instagram",
   },
   {
     key: "3",
     label: "Twitter",
-    icon: <FontAwesomeIcon icon={faFireBurner} color="white"  />,
+    icon: <FontAwesomeIcon icon={faTwitter} color="white" size="2x" />,
     path: "/twitter",
   },
   {
     key: "4",
     label: "Google",
-    icon: <FontAwesomeIcon icon={faAddressCard} color="white"  />,
+    icon: <FontAwesomeIcon icon={faGoogle} color="white"  size="2x"/>,
     path: "/google",
   },
   {
     key: "5",
-    label: "Youtube",
-    icon: <FontAwesomeIcon icon={faPhone} color="white"  />,
+    label: "YouTube",
+    icon: <FontAwesomeIcon icon={faYoutube} color="white"  size="2x"/>,
     path: "/youtube",
   },
 ];
