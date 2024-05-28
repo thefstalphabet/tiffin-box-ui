fa65f5bcd1a8e1ef28a01aa1f1b73a702c4ff08a
Akash patel <akashpatel10000@gmail.com>
2024-05-28 17:34:38 +0530

    Update: added kitchen signup route

---
+++ src/Routes/KitchenSignup/KitchenSignup.tsx
@@ -0,0 +1,10 @@
+import * as Styles from "./KitchenSignupStyles";
+import { KitchenSignupForm } from "../../Components";
+
+export default function KitchenSignup() {
+  return (
+    <Styles.Container>
+      <KitchenSignupForm />
+    </Styles.Container>
+  );
+}
