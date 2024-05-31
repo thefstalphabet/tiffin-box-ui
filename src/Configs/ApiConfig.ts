324b1bd86b5387c0eac16f51836e92bb5932c07e
Akash patel <akashpatel10000@gmail.com>
2024-05-31 21:15:39 +0530

    Update: deployment updates

--- src/Configs/ApiConfig.ts
+++ src/Configs/ApiConfig.ts
@@ -1,7 +1,5 @@
 export const getApiUrl = () => {
-    console.log(process.env.REACT_APP_IS_DEV);
-    
-    if (process.env.REACT_APP_IS_DEV) {
+    if (window.location.hostname === "localhost") {
         return "http://localhost:4000/api"
     } else {
         return process.env.REACT_APP_API_URL;
