fa149d80f744c6d3ac23f343abfb328c1fe3f138
Akash patel <akashpatel10000@gmail.com>
2024-08-24 13:38:03 +0530

    Updated: fix import bug

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -3,9 +3,7 @@ import chef from "../Assets/Images/chef.jpg";
 import logoWithText from "./Logos/logo-no-background.png";
 import pageNotfoundImage from "../Assets/Images/pageNotfoundImage.jpg";
 import homeImg from "../Assets/Images/homeImg.jpg";
-import kitchen from "../Assets/Images/kitchen.png";
 import logo from "./Logos/logo.png";
-import reason from "../Assets/Images/reason.jpg";
 import Increased from "../Assets/Images/Increased.jpg";
 import AdditionalRevenue from "../Assets/Images/AdditionalRevenue.jpg";
 import Custumer from "../Assets/Images/Custumer.jpg";
@@ -17,9 +15,7 @@ export {
   chef,
   pageNotfoundImage,
   homeImg,
-  kitchen,
   logoWithText,
-  reason,
   Increased,
   AdditionalRevenue,
   Custumer,
